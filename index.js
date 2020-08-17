0da3f84f5c9c655de3812c9a319a4613fa86a352
Wei Shi <ailswanster@gmail.com>
2020-08-17 21:17:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,21 +17,15 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
     return "I can't hear you!";
-  } 
-}
-
-
-function sayHiToGrandma(string) {
-  if (string === shout(string)) {
+  }else if (string === shout(string)) {
     return "YES INDEED!"; 
   }
-}
- 
-function  sayHiToGrandma(string) {
-  sayHiToGrandma(string);
-  sayHiToGrandma(string); 
-  if (string === "I love you, Grandma.") {
+  
+   if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
 }
+
+
+  
   
